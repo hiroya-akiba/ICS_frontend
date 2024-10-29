133db1dc415ae02f72938c29425a2c98c3715dca
hiroya-akiba <hiroya0504@gmail.com>
2024-10-29 13:38:45 +0000

    2024/10/29 商品を追加するフォームを追加する

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import productsData from "./sample/dummy_products.json";
 import Link from "next/link";
 
@@ -15,15 +15,42 @@ export default function Page(){
     const [data, setData] = useState<Array<ProductData>>([]);
 
     useEffect(() => {
-        setData(productsData);
+        setData(productsData); //importしたjsonを定義したステートフル変数に入れる
     }, [])
+
+    // 新規登録処理、新規登録行の表示状態を保持
+    const [shownNewRow, setShownNewRow] = useState(false);
+    const handleShowNewRow = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setShownNewRow(true)
+    };
+    const handleAddCancel = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setShownNewRow(false)
+    };
+    const handleAdd = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setShownNewRow(false)
+    };
 
     return(
         <>
             <h2>商品一覧</h2>
             <p>商品の一覧を表示します</p>
+            <button onClick={ handleShowNewRow }>商品を追加する</button>
             <table>
                 <tbody>
+                    {shownNewRow ? (
+                        <tr>
+                            <td></td>
+                            <td><input type="text" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="text" /></td>
+                            <td></td>
+                        </tr>
+                    ): ""}
+
+                    {/*setDataで代入したステートフル変数をforEachで回す*/}
                     {data.map((data:any) => (
                         <tr key={data.id}>
                             <td>{data.id}</td>
